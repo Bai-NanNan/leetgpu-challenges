247702eddc09e2d879327599b531f282a276a802
Bai-NanNan <1653711769@qq.com>
2026-07-26 20:23:20 +0800

    矩阵加法

--- challenges/easy/7_color_inversion/starter/starter.cu
+++ challenges/easy/7_color_inversion/starter/starter.cu
@@ -4,7 +4,7 @@ __global__ void invert_kernel(unsigned char* image, int width, int height){
     //像素索引和像素总量
     int pixel_idx = blockIdx.x * blockDim.x + threadIdx.x;
     int pixel_count = width * height;
-    if(pixel_idx>pixel_count) return;
+    if(pixel_idx>=pixel_count) return;
     uchar4* pixels = reinterpret_cast<uchar4*>(image);
     uchar4 pixel = pixels[pixel_idx];
     pixel.x = 255 - pixel.x;
--- challenges/easy/8_matrix_addition/starter/starter.cu
+++ challenges/easy/8_matrix_addition/starter/starter.cu
@@ -1,12 +1,54 @@
 #include <cuda_runtime.h>
 
-__global__ void matrix_add(const float* A, const float* B, float* C, int N) {}
+__global__ void matrix_add(const float* __restrict__ A, const float* __restrict__ B, float* C, int N) {
+    int index = blockIdx.x * blockDim.x + threadIdx.x;
+    int total = N * N;
+    if (index < total) {
+        C[index] = A[index] + B[index];
+    }
+    //Benchmark   median 1.1787 ms  min 1.1557 ms  p20 1.1708 ms  p80 1.2034 ms
+}
+
+//固定尺寸的Grid循环完成计算，且float4访存
+__global__ void matrix_add_GridStridLoop_float4(
+    const float4* __restrict__ A, 
+    const float4* __restrict__ B, 
+    float4* C, 
+    int vector_count) {
+    int index = blockIdx.x * blockDim.x + threadIdx.x;
+    int stride = blockDim.x * gridDim.x;//每步跨越Thread总数
+    for(;index<vector_count;index += stride){
+        float4 a = A[index];
+        float4 b = B[index];
+
+        C[index] = make_float4(
+            a.x + b.x,
+            a.y + b.y,
+            a.z + b.z,
+            a.w + b.w
+        );
+    }
+    //Benchmark   median 0.9398 ms  min 0.9272 ms  p20 0.9313 ms  p80 0.9573 ms
+
+}
 
 // A, B, C are device pointers (i.e. pointers to memory on the GPU)
 extern "C" void solve(const float* A, const float* B, float* C, int N) {
-    int threadsPerBlock = 256;
-    int blocksPerGrid = (N * N + threadsPerBlock - 1) / threadsPerBlock;
+    // int threadsPerBlock = 256;
+    // int blocksPerGrid = (N * N + threadsPerBlock - 1) / threadsPerBlock;
+    // matrix_add<<<blocksPerGrid, threadsPerBlock>>>(A, B, C, N);
+    int total = N*N;
+    int vector_count = (total+3) / 4;
+    constexpr int threads = 256;
+    constexpr int blocks = 256;
+    matrix_add_GridStridLoop_float4<<<blocks, threads>>>(
+        reinterpret_cast<const float4*>(A),
+        reinterpret_cast<const float4*>(B),
+        reinterpret_cast<float4*>(C),
+        vector_count
+    );
 
-    matrix_add<<<blocksPerGrid, threadsPerBlock>>>(A, B, C, N);
+
+    
     cudaDeviceSynchronize();
 }
