8d463cf644447519fc4c3dce187b5d2c96520523
Bai-NanNan <1653711769@qq.com>
2026-07-26 18:36:36 +0800

    颜色反转

--- challenges/easy/7_color_inversion/starter/starter.cu
+++ challenges/easy/7_color_inversion/starter/starter.cu
@@ -1,6 +1,17 @@
 #include <cuda_runtime.h>
 
-__global__ void invert_kernel(unsigned char* image, int width, int height) {}
+__global__ void invert_kernel(unsigned char* image, int width, int height){
+    //像素索引和像素总量
+    int pixel_idx = blockIdx.x * blockDim.x + threadIdx.x;
+    int pixel_count = width * height;
+    if(pixel_idx>pixel_count) return;
+    uchar4* pixels = reinterpret_cast<uchar4*>(image);
+    uchar4 pixel = pixels[pixel_idx];
+    pixel.x = 255 - pixel.x;
+    pixel.y = 255 - pixel.y;
+    pixel.z = 255 - pixel.z;
+    pixels[pixel_idx] = pixel;
+}
 // image_input, image_output are device pointers (i.e. pointers to memory on the GPU)
 extern "C" void solve(unsigned char* image, int width, int height) {
     int threadsPerBlock = 256;
